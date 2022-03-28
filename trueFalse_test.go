9088b79069bb041e786a2bf8214725282fcd59c1
ILikeTea <ybingcheng@gmail.com>
2022-03-28 17:02:39 +0800

    update test

--- trueFalse_test.go
+++ trueFalse_test.go
@@ -6,19 +6,19 @@ import (
 
 func TestTrueFalse(t *testing.T) {
 	got := compare(0)
-	res := "Too low, try again next time!"
+	res := "Smaller than secret value"
 	if got != res {
 		t.Errorf("compare(0) = %s; want %s got %s", got, res, got)
 	}
 
 	got = compare(90)
-	res = "Too high, try again next time!"
+	res = "Bigger than secret value"
 	if got != res {
 		t.Errorf("compare(90) = %s; want %s got %s", got, res, got)
 	}
 
 	got = compare(88)
-	res = "Well Done! Your guess is correct"
+	res = "Same as secret value"
 	if got != res {
 		t.Errorf("compare(88) = %s; want %s got %s", got, res, got)
 	}
